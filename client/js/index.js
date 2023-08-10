1b95159daab8af71017d4dd77aaca5ff72c43802
nvdinh185 <nvdinh0766777123@gmail.com>
2023-08-10 12:09:57 +0700

    x

---
+++ client/js/index.js
@@ -0,0 +1,7 @@
+var listFr = $('#list-friends');
+
+var h1Element = $('<h1 class="title">Những người bạn111</h1>');
+console.log(h1Element);
+
+listFr.append('<h1 class="title">Những người bạn111</h1>');
+//listFr.html('<h1 class="title">Những người bạn</h1>');
