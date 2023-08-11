32c6d65505771928894bf1b47cc8f1298774fcb3
nvdinh185 <nvdinh0766777123@gmail.com>
2023-08-11 20:02:55 +0700

    rv

--- client/js/index.js
+++ client/js/index.js
@@ -14,12 +14,12 @@ async function display() {
             var divElement = $('<div class="content-grid-sec"></div>');
             divElement.html(
                 `<div class="content-sec-info">
-                <h3><a href="chi-tiet.html?did=${fr.id}">${fr.name}</a></h3>
-                <h4>Đăng ngày: ${fr.date_create} - Lượt xem: ${fr.counter}</h4>
-                <p>${fr.description}</p>
-                <img src="images/${fr.image}" alt="" />
-                <a class="bttn" href="chi-tiet.html?did=${fr.id}">Chi tiết bạn tôi</a>
-            </div>`
+                    <h3><a href="chi-tiet.html?did=${fr.id}">${fr.name}</a></h3>
+                    <h4>Đăng ngày: ${fr.date_create} - Lượt xem: ${fr.counter}</h4>
+                    <p>${fr.description}</p>
+                    <img src="images/${fr.image}" alt="" />
+                    <a class="bttn" href="chi-tiet.html?did=${fr.id}">Chi tiết bạn tôi</a>
+                </div>`
             );
 
             listElement.append(divElement);
--- middleware/Middleware.js
+++ middleware/Middleware.js
@@ -13,9 +13,6 @@ class Middleware {
             if (err) {
                 res.sendStatus(500);
             } else {
-                // for (var key in fields) {
-                //     formData[key] = fields[key];
-                // }
                 formData = fields;
                 var isSelectedFile = !(Object.entries(files).length === 0 && files.constructor === Object);
                 var key = "file";
